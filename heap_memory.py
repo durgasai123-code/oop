2f044cbf608dc3081bae1764d3d895302abb7c8d
durgasai123-code <66827480+durgasai123-code@users.noreply.github.com>
2020-11-08 00:06:32 +0530

    Update heap_memory.py

--- heap_memory.py
+++ heap_memory.py
@@ -3,6 +3,8 @@
 # sizwe of object depends upon no of variables we have
 # but who responsible to caliculate and assign that memory
 # constrctor is responsible for that.
+# Constructors are generally used for instantiating an object.The task of constructors is to initialize(assign values) to the data members of the class when an object of class is created.
+# In Python the __init__() method is called the constructor and is always called when an object is created.
 # self is reference variable it refers the object
 class heap:
     pass
